bc17ab7372924a4f006c95cd9ca50a24b00de007
shailender1911 <kumarjase43@gmail.com>
2020-02-01 23:10:05 +0530

    working on spring project

---
+++ spring-demo-one/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach{
+
+
+	public String getDailyWorkOut() {
+		// TODO Auto-generated method stub
+		return "Do at least 100 push ups daily";
+	}
+
+	
+					
+
+}
